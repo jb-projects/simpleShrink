a97ad7a70ec42110690c2cdf81d4bda3b6fe92de
Jesse Bruhn <jmb112485@gmail.com>
2020-06-11 13:43:50 -0400

    adding shell for baseball example

---
+++ examples/baseball_example.R
@@ -0,0 +1,37 @@
+# DOCUMENTATION -----------------------------------------------------------
+
+#   Author:     Jesse Bruhn
+#   Contact:    jbruhn@princeton.edu
+
+# PREAMBLE ----------------------------------------------------------------
+
+#Clear workspace
+rm(list=ls())
+
+#Load Packages
+pckgs <- c("tidyverse")
+lapply(pckgs, library, character.only=TRUE)
+
+#Set Options
+options(scipen=100)
+
+#Clean Workspace
+rm(pckgs)
+
+#Session Info for Reproducibility
+Sys.Date()
+sessionInfo()
+
+# NOTES -------------------------------------------------------------------
+
+
+
+
+
+# EXAMPLE -----------------------------------------------------------------
+
+
+# SYNTAX FOR R PACKAGE ----------------------------------------------------
+
+
+
